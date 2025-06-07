4e7791b4525ff5f0bd8ec99087fa4ea3141e7bd8
ShadowLoba415 <erica@aubriannakay.com>
2025-06-07 10:15:22 -0600

    Added function displaypoem and generatePoem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey ="c41ea0t0206a28395238ff3c5b4e7o2c";
+  let prompt = "";
+  let context = "";
+  let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&$context=${context}&key=${apikey}'
+  // Make a call to the API
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
