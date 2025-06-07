72075af724cdb7d5121361eef77475ea0feee168
ShadowLoba415 <erica@aubriannakay.com>
2025-06-07 10:41:02 -0600

    index.js added instructionInput, apikey, context, and console.log

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function displayPoem(response) {
+
+  console.log("poem generated");  
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -10,11 +12,16 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey ="c41ea0t0206a28395238ff3c5b4e7o2c";
-  let prompt = "";
-  let context = "";
-  let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&$context=${context}&key=${apikey}'
-  // Make a call to the API
+  let context = 
+  "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions";
+   let prompt = 'User instructions: Generate a French poem about ${instructionsInput.value}';
+  let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&$context=${context}&key=${apikey}';
+  
+  console.log("Generating poem");
+  console.log('Prompt: ${prompt}');
+  console.log('Context: ${context}');
 
   axios.get(apiURL).then(displayPoem);
 }
